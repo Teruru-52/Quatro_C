d1588aac0849c9bba5752768fceadcd2cde66502
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-01-25 13:59:11 +0900

    D制御における1次遅れフィルタの修正とPID制御における角度の扱いをradにした

--- Core/Src/control.c
+++ Core/Src/control.c
@@ -1,9 +1,10 @@
 #include "control.h"
 
-static float pre_error = 0.0;
+static float pre_error;
 static float sum_error = 0.0;
-static float pre_error2 = 0.0;
+static float pre_error2;
 static float sum_error2 = 0.0;
+static float pre_deriv2 = 0.0;
 
 // static float dt_recip;  // 1/sampling time
 
@@ -22,7 +23,7 @@ void PIDControlInit(Control_Typedef *pid){
 
 void AngleControl(Gyro_Typedef *gyro, Control_Typedef *pid){
   float error, deriv;
-  error = pid->ref - gyro->yaw;
+  error = (pid->ref - gyro->yaw) * M_PI / 180;
   sum_error += error*pid->ts;
   deriv = (error - pre_error)/pid->ts;
   pid->ref2 = pid->kp1*error + pid->ki1*sum_error + pid->kd1*deriv;
@@ -31,9 +32,10 @@ void AngleControl(Gyro_Typedef *gyro, Control_Typedef *pid){
 
 void AngularVelocityControl(Gyro_Typedef *gyro, Control_Typedef *pid){
   float error2, deriv2;
-  error2 = pid->ref2 - gyro->gz;
+  error2 = (pid->ref2 - gyro->gz) * M_PI / 180;
   sum_error2 += error2*pid->ts;
-  deriv2 = (error2 - pre_error2)/pid->ts*D_FILTER_COFF;
+  deriv2 = (error2 - pre_error2)/pid->ts;
+  deriv2 = pre_deriv2 + (deriv2 - pre_deriv2)*D_FILTER_COFF;
   pid->input = pid->kp2*error2 + pid->ki2*sum_error2 + pid->kd2*deriv2;
   if(pid->input > 0){
     __HAL_TIM_SET_COMPARE(&htim5, TIM_CHANNEL_1, STANDARD_INPUT+pid->input);
@@ -48,4 +50,5 @@ void AngularVelocityControl(Gyro_Typedef *gyro, Control_Typedef *pid){
     __HAL_TIM_SET_COMPARE(&htim5, TIM_CHANNEL_4, STANDARD_INPUT-pid->input);
   }
   pre_error2 = error2;
+  deriv2 = pre_deriv2;
 }
